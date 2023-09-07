b4024803be836ab8af33380ac695d660952ff239
josue rushanika <100031798+josuerushanika@users.noreply.github.com>
2023-09-07 20:51:43 +0200

    Update coding.js

--- coding.js
+++ coding.js
@@ -1,1 +1,20 @@
+/*// BIG O
+It tell us how the problem will be solved 
 
+What is good code ? it must be :
+1.Readable
+2.Scalable
+
+
+*/
+const nemo = ['nemo'];
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+        console.log('Found NEMO !');
+      }
+  }
+}
+
+findNemo(nemo);  
